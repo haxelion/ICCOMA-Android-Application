b1184364e74156a73a4181d620798eab3c9d82fd
Charles Hubain <haxelion@gmail.com>
2014-01-28 00:38:50 +0100

    Fixed crash when closing the application.

--- src/com/haxelion/iccoma/MainActivity.java
+++ src/com/haxelion/iccoma/MainActivity.java
@@ -59,13 +59,6 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener, O
 	}
 
 	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		client.stop();
-		statusDaemonTimer.cancel();
-	}
-
-	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
